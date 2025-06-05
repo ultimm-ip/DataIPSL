759eb3f11d0d155830441329b53ac94b314d5af4
Mahim <mishra.mahim@gmail.com>
2025-06-05 18:21:04 +0530

    updated logo and youtube links

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,10 @@ Author: GrayGrids
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/GIPF_Logo.jpg';
+          logo.src = 'assets/images/ultim/ultim_logo.JPG';
         } else {
           header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/GIPF_Logo.jpg';
+          logo.src = 'assets/images/ultim/ultim_logo.JPG';
         }
 
         // show or hide the back-top-top button
